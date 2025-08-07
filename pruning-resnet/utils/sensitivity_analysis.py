ce542ed53b3c4eab77c37b8d45a483966f4db12a
Sayyed Farrukh Mehmood <153420545+sfarrukhm@users.noreply.github.com>
2025-08-07 20:29:26 +0500

    Update sensitivity_analysis.py

--- pruning-resnet/utils/sensitivity_analysis.py
+++ pruning-resnet/utils/sensitivity_analysis.py
@@ -68,29 +68,4 @@ def sensitivity_scan(model, dataloader, scan_step=0.1, scan_start=0.4, scan_end=
         accuracies.append(accuracy)
 
     return sparsities, accuracies
-def plot_sensitivity_scan(sparsities, accuracies, dense_model_accuracy):
-    lower_bound_accuracy = 100 - (100 - dense_model_accuracy) * 1.5
-    fig, axes = plt.subplots(3, int(math.ceil(len(accuracies) / 3)),figsize=(15,8))
-    axes = axes.ravel()
-    plot_index = 0
-    for name, param in model.named_parameters():
-        if param.dim() > 1:
-            ax = axes[plot_index]
-            curve = ax.plot(sparsities, accuracies[plot_index])
-            line = ax.plot(sparsities, [lower_bound_accuracy] * len(sparsities))
-            ax.set_xticks(np.arange(start=0.4, stop=1.0, step=0.1))
-            ax.set_ylim(80, 95)
-            ax.set_title(name)
-            ax.set_xlabel('sparsity')
-            ax.set_ylabel('top-1 accuracy')
-            ax.legend([
-                'accuracy after pruning',
-                f'{lower_bound_accuracy / dense_model_accuracy * 100:.0f}% of dense model accuracy'
-            ])
-            ax.grid(axis='x')
-            plot_index += 1
-    fig.suptitle('Sensitivity Curves: Validation Accuracy vs. Pruning Sparsity')
-    fig.tight_layout()
-    fig.subplots_adjust(top=0.925)
-    plt.show()
 
